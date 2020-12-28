f4aa1efb372c7a685d27a7dcdc9a75371ca9d88d
reza <r.zerang42@gmail.com>
2020-12-28 21:22:06 +0330

    update
    first update

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+function clockFunction(){
+    var date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+
+    let hrPosition = hours*360/12;
+    let minPosition = minutes*360/60;
+    let secPosition = seconds*360/60;
+
+
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+    console.log(hrPosition)
+
+
+}
+
+var interval = setInterval (clockFunction, 1000);
