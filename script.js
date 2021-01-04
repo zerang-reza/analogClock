36ffd42b23736507a15164eeb9ded1d1485339de
reza <r.zerang42@gmail.com>
2021-01-04 21:45:57 +0330

    accurate handels
    making hour and minute handels accurate.

--- script.js
+++ script.js
@@ -4,24 +4,20 @@ const SECONDHAND = document.querySelector("#second");
 
 
 function clockFunction(){
+    
     var date = new Date();
 
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-
-    let hrPosition = hours*360/12;
-    let minPosition = minutes*360/60;
+    let hrPosition = (hours*360/12) + ((minutes*360/60)/12);
+    let minPosition = (minutes*360/60) + ((seconds*360/60)/60);
     let secPosition = seconds*360/60;
-
-
 
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-
-    console.log(hrPosition)
 
 
 }
